147b87c55e15a4a75f157976b776f4c86a1a13dc
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-07-22 15:45:19 +0530

    add listener to submit button

--- script/wordle.js
+++ script/wordle.js
@@ -1,3 +1,15 @@
-const main = () => {};
+const onSubmit = () => {};
+
+const main = () => {
+  const submitBtn = document.querySelector("#submit-btn");
+  const guessBox = document.querySelector("#secret-word");
+
+  const secretWord = "TIGER";
+
+  submitBtn.onclick = () => {
+    const userGuess = guessBox.value;
+    console.log(userGuess);
+  };
+};
 
 window.onload = main;
