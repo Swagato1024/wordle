8050cefe355169283aba8369098e5a542db989bd
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-07-22 16:17:51 +0530

    add functionality to disable input box after 5 letters

--- script/wordle.js
+++ script/wordle.js
@@ -2,12 +2,22 @@ const displayResult = (resultBox, result) => {
   resultBox.innerText = `${result} Guess`;
 };
 
+const setUpGuessBox = (guessBox) => {
+  guessBox.oninput = () => {
+    if (guessBox.value.length === 5) {
+      guessBox.disabled = true;
+    }
+  };
+};
+
 const main = () => {
   const submitBtn = document.querySelector("#submit-btn");
   const guessBox = document.querySelector("#secret-word");
   const resultBox = document.querySelector(".result");
 
   const secretWord = "TIGER";
+
+  setUpGuessBox(guessBox);
 
   submitBtn.onclick = () => {
     const userGuess = guessBox.value;
