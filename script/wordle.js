b31f7e1d2102af369fd5bb02221091dcc32b246c
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-07-22 16:42:19 +0530

    add GameController class

--- script/wordle.js
+++ script/wordle.js
@@ -10,6 +10,33 @@ const setUpGuessBox = (guessBox) => {
   };
 };
 
+class Player {
+  #guesses;
+
+  constructor() {
+    this.#guesses = [];
+  }
+}
+
+class View {
+  #resultBox;
+
+  constructor(resultBox) {
+    this.#resultBox = resultBox;
+  }
+}
+
+class GameController {
+  #player;
+  #view;
+  #secretWord;
+
+  constructor(player, view) {
+    this.#player = player;
+    this.#view = view;
+  }
+}
+
 const main = () => {
   const submitBtn = document.querySelector("#submit-btn");
   const guessBox = document.querySelector("#guess-box");
@@ -18,6 +45,11 @@ const main = () => {
   const secretWord = "TIGER";
 
   setUpGuessBox(guessBox);
+
+  const player = new Player();
+  const view = new View(resultBox);
+
+  const gameController = new GameController(player, view);
 
   submitBtn.onclick = () => {
     const userGuess = guessBox.value;
