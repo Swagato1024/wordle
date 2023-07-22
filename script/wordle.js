ce321fcd8d907c4ec825c1aedf8adef1f0a6e84f
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-07-22 15:51:46 +0530

    add functionality to show whether the guess is correct

--- script/wordle.js
+++ script/wordle.js
@@ -1,14 +1,19 @@
-const onSubmit = () => {};
+const showResult = (resultBox, result) => {
+  resultBox.innerText = result;
+};
 
 const main = () => {
   const submitBtn = document.querySelector("#submit-btn");
   const guessBox = document.querySelector("#secret-word");
+  const resultBox = document.querySelector(".result");
 
   const secretWord = "TIGER";
 
   submitBtn.onclick = () => {
     const userGuess = guessBox.value;
-    console.log(userGuess);
+    const result = userGuess === secretWord ? "Correct" : "Incorrect";
+
+    showResult(resultBox, result);
   };
 };
 
