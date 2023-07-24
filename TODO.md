c609e7a82bb5ec830c896175f307a3cd6186e677
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-07-24 17:36:09 +0530

    add feature to pick a secret word randomly

--- TODO.md
+++ TODO.md
@@ -1,3 +1,17 @@
-1. keep stats for each guess
-2. don't remove children
-3. just append
+1.  keep stats for each guess
+2.  don't remove children
+3.  just append
+
+          <div id="attempt-left"></div>
+
+          occur
+
+    [
+    "faint",
+    "waste",
+    "empty",
+    "blame",
+    "creep",
+    "shift",
+    "abuse"
+    ]
--- script/wordle.js
+++ script/wordle.js
@@ -1,15 +1,40 @@
-const main = () => {
+const pickRandomWord = () => {
+  const commonWords = [
+    "faint",
+    "tiger",
+    "empty",
+    "blame",
+    "creep",
+    "shift",
+    "abuse",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * commonWords.length);
+
+  return new Word(commonWords[randomIndex]);
+};
+
+const createInputController = () => {
   const submitBtn = document.querySelector("#submit-btn");
   const guessBox = document.querySelector("#guess-box");
-  const resultBox = document.querySelector(".result-container");
-  const attemptsLeft = document.querySelector("#attempt-left");
 
-  const secretWord = new Word("rover");
+  return new InputController(guessBox, submitBtn);
+};
+
+const createView = () => {
+  const resultBox = document.querySelector(".result-container");
+  const attemptsContainer = document.querySelector("#attempt-left");
+
+  return new View(resultBox, attemptsContainer);
+};
+
+const main = () => {
+  const secretWord = pickRandomWord();
   const guesses = new Guesses(secretWord);
   const game = new Game(guesses, 6);
-  const view = new View(resultBox, attemptsLeft);
+  const view = createView();
 
-  const inputController = new InputController(guessBox, submitBtn);
+  const inputController = createInputController();
   const gameController = new GameController(game, inputController, view);
 
   gameController.start();
